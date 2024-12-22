383bea9d9a8823d3b596aa5a025c735bdbcd625c
ganesh <ganeshmehra776@gmail.com>
2024-12-22 14:00:08 +0530

    add json

--- src/jsonFile/Pro.jsx
+++ src/jsonFile/Pro.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 // import arr from "./data1"
-// import arr from "./Jsonfile.json"
-// import Pro from "./Pro2"
-import Pro2 from './Pro2'
+import arr from "./Jsonfile.json";
+import Pro2 from './Pro2';
 
-const Pro = ({second,setsecond}) => {
-   
-   
-    return (
-        <>
-<Pro2  second={second} setsecond={setsecond}/>
-        </>)
+const Pro = ({ second, setsecond }) => {
+  useEffect(() => {
+    // Check if data is already in localStorage
+    if (!localStorage.getItem("update-data")) {
+      // Store the JSON data in localStorage
+      localStorage.setItem("update-data", JSON.stringify(arr));
+    }
+  }, []); // Empty dependency array ensures this runs once on component mount
+
+  return (
+    <>
+      <Pro2 second={second} setsecond={setsecond} />
+    </>
+  );
 }
 
-export default Pro
+export default Pro;
